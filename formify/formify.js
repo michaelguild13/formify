f8b0244be86ee6a2544c5cf3a7f46eac1cf3d4ac
Michael Guild <michaelguild13@gmail.com>
2015-10-01 10:51:35 -0400

    new direction of form first

---
+++ formify/formify.js
@@ -0,0 +1,22 @@
+var formify = function () {
+  "use strict";
+  // find formify
+  // build virtual form for submit
+  // create formify form
+  // create copy of formify model
+
+  var form = document.getElementById('formify');
+
+  function init () {
+  }
+
+  function initInputs ( form ) {
+
+  }
+
+  function createInput ( el, className ) {
+
+  }
+
+  init();
+};
