97a59e66cd1f303ea626e1fd105babef3abc1fe6
Michael Guild <michaelguild13@gmail.com>
2015-10-01 14:38:33 -0400

    working on formify

--- formify/formify.js
+++ formify/formify.js
@@ -4,19 +4,41 @@ var formify = function () {
   // build virtual form for submit
   // create formify form
   // create copy of formify model
-
   var form = document.getElementById('formify');
+  var i = form.length;
 
-  function init () {
+  while ( i-- ) {
+    var input = form[i];
+    input.setAttribute('style', 'display: none;');
+
+    if (input.type !== 'submit' && input.type !== 'reset' && input.type !== 'button' && input.type !== 'output') {
+      input.parentNode.appendChild(creatContext(input))
+        .addEventListener('click', showInputs);
+    }
   }
 
-  function initInputs ( form ) {
+  function showInputs () {
+    console.log(this);
+    var i = this.parentNode.children.length
+    this.setAttribute('style', 'display: none;');
+
+    while ( i-- ) {
+      var child = this.parentNode.children[i];
+      if ( child.className !== 'formify-input' ) {
+        child.setAttribute('style', 'display: block;');
+        child.tagName === 'INPUT' || child.tagName === 'TEXTAREA' ? child.focus():'';
+      }
+    }
+  }
+
+  function hideInputs () {
 
   }
 
-  function createInput ( el, className ) {
-
+  function creatContext (el) {
+    var content = document.createElement('span');
+    content.className = 'formify-input';
+    content.innerText = el.value;
+    return content;
   }
-
-  init();
 };
