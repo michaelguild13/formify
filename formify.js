49ed2fed206ca0001d68454a1674113978368297
Michael Guild <michaelguild13@gmail.com>
2015-10-05 11:33:08 -0400

    code cleaning

--- formify.js
+++ formify.js
@@ -1,92 +1,73 @@
 var Formify = ( function () {
   "use strict";
-  // find formify
-  // build virtual form for submit
-  // create formify form
-  // create copy of formify model
-  // #TODO: return object
-  var form = document.getElementById('formify');
-  var inputFactory = {
-    submit: function () {},
-    reset: function () {},
-    button: function () {},
-    output: function () {},
-    radio: function () {},
-    checkbox: function () {},
-    range: inputInit,
-    number: inputInit,
-    url: inputInit,
-    date: inputInit,
-    email: inputInit,
-    tel: inputInit,
-    text: inputInit,
-    textarea: inputInit
-  };
+
+  var form = document.getElementById('formify'),
+      inputFactory = {
+        submit: function () {},
+        reset: function () {},
+        button: function () {},
+        output: function () {},
+        radio: function () {},
+        checkbox: function () {},
+        range: function () {},
+        number: inputInit,
+        url: inputInit,
+        date: inputInit,
+        email: inputInit,
+        tel: inputInit,
+        text: inputInit,
+        textarea: inputInit
+      };
 
   var Formify = {
     init: function () {
       let i = form.length;
-
       while ( i-- ) {
         let input = form[i];
-        input.setAttribute('style', 'display: none;');
-        input.addEventListener('blur', hideInput);
-        input.formifyContent = creatContext(input);
         inputFactory[input.type](input);
-        // if (input.type !== 'submit' && input.type !== 'reset' && input.type !== 'button' && input.type !== 'output') {
-        //   input.parentNode
-        //     .insertBefore(input.formifyContent, input.nextSibling)
-        //     .addEventListener('click', showInput);
-        // }
       }
     },
 
     showForm: function () {
       let i = form.length;
-
       while ( i-- ) {
-        form[i].setAttribute('style', 'display: block;');
+        form[i].style.display = 'block';
       }
     },
 
     hideForm: function () {
       let i = form.length;
       while ( i-- ) {
-        form[i].setAttribute('style', 'display: none;');
+        form[i].style.display = 'none'
       }
     }
   };
 
   function showInput () {
-    let i = this.parentNode.children.length;
-    this.setAttribute('style', 'display: none;');
+    this.style.display = 'none';
+    this.formifyInput.style.display = 'blocK';
+  }
 
-    while ( i-- ) {
-      let child = this.parentNode.children[i];
-      if ( child === this.formifyInput ) {
-        child.setAttribute('style', 'display: block;');
-        child.focus();
-      } else if ( this.formifyInput.type === 'range') {
-        form.querySelector('[for=' + this.formifyInput.id +']').setAttribute('style', 'display: block;');
-      }
+  function showRange () {
+    if ( this.formifyInput.type === 'range') {
+      form.querySelector('[for=' + this.formifyInput.id +']').style.display = 'block';
     }
   }
 
   function hideInput () {
     let val = this.value;
+    this.style.display = 'none';
+    this.formifyContent.style.display = 'block';
+    this.formifyContent.innerText = val || this.placeholder || 'Put Content Here';
+  }
 
-    this.setAttribute('style', 'display: none;');
-
+  function hideRange () {
     if ( this.type === 'range') {
       let output = form.querySelector('[for=' + this.id +']');
       output.setAttribute('style', 'display: none;');
     }
-
-    this.formifyContent.setAttribute('style', 'display: block;');
-    this.formifyContent.innerText = val || this.placeholder || 'Put Content Here';
   }
-
-  function creatContext (el) {
+  function createContext (el) {
     let content = document.createElement('span');
     content.className = 'formify-input';
     content.innerText = el.value;
@@ -95,9 +76,16 @@ var Formify = ( function () {
   }
 
   function inputInit (input) {
+    input.style.display = 'none';
+    input.formifyContent = createContext(input);
+    input.addEventListener('blur', hideInput);
     input.parentNode
       .insertBefore(input.formifyContent, input.nextSibling)
       .addEventListener('click', showInput);
+  }
+
+  function multiInputInit (input) {
+
   }
 
   return Formify;
