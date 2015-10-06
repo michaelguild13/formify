5fc7e0fd43d000dad7109f6f01ac64a121cff49d
Michael Guild <michaelguild13@gmail.com>
2015-10-06 09:00:27 -0400

    content editable & textareas

--- content/formify.js
+++ content/formify.js
@@ -2,20 +2,20 @@ var Formify = ( function () {
   "use strict";
 
   var inputFactory = {
-        submit: function () {},
-        reset: function () {},
-        button: function () {},
-        output: function () {},
-        radio: function () {},
-        checkbox: function () {},
-        range: function () {},
-        number: function () {},
-        url: function () {},
-        date: function () {},
-        email: function () {},
-        tel: function () {},
-        text: function (input) { console.log(input);},
-        textarea: function () {}
+        submit: function (input) {},
+        reset: function (input) {},
+        button: function (input) {},
+        radio: function (input) {},
+        checkbox: function (input) {},
+        range: function (input) {},
+        number: function (input) {input.appendChild(createInput(input, 'number'));},
+        url: function (input) {input.appendChild(createInput(input, 'url'));},
+        date: function (input) {input.appendChild(createInput(input, 'date'));},
+        email: function (input) {input.appendChild(createInput(input, 'email'));},
+        tel: function (input) { input.appendChild(createInput(input, 'tel'));},
+        text: function (input) { input.appendChild(createInput(input, 'text'));},
+        textarea: function (input) { input.appendChild(createTextArea(input, 'textarea'));},
+        contenteditable: function (input) { createTextArea(input);}
       };
 
   var Formify = {
@@ -41,6 +41,36 @@ var Formify = ( function () {
     }
   };
 
+function createInput( el, type ) {
+  let input = document.createElement('input'),
+    context = el.innerText;
+
+  el.innerHTML = '<span>' + context + '</span>';
+  input.type = type;
+  input.value = context;
+  input.addEventListener("change", function () {
+    el.firstChild.innerText = input.value;
+  }, false);
+
+  return input;
+}
+
+function createTextArea( el, type ) {
+  let input = '',
+    context = el.innerHTML;
+
+  if ( type === 'textarea') {
+    input = document.createElement('textarea');
+    el.innerHTML = '<span>' + context + '</span>';
+    input.value = context;
+    input.addEventListener("change", function() {
+      el.firstChild.innerHTML = input.value;
+    }, false);
+  } else {
+    el.contentEditable = true;
+  }
+  return input;
+}
 
   return Formify;
 }());
